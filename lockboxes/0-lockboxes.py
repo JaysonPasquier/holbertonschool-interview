5a4b000f8d76b965f920b3462e87e37c65bc24e6
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-12-05 03:12:14 -0500

    python for the first task

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+    """Determine if all boxes can be opened."""
+    if not boxes or not isinstance(boxes, list):
+        return False
+
+    n = len(boxes)
+    opened = set([0])
+    keys = list(boxes[0])
+
+    while keys:
+        key = keys.pop()
+
+        if key < n and key not in opened:
+            opened.add(key)
+            for new_key in boxes[key]:
+                if new_key not in opened:
+                    keys.append(new_key)
+
+    return len(opened) == n
