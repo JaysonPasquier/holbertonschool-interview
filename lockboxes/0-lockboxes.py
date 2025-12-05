fe6a0034146895ca02654e336a7b49b4fe402821
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-12-05 03:20:59 -0500

    python for the first task

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
+"""
+Module: 0-lockboxes
+Determines if all boxes in a list can be unlocked.
+"""
+
+
 def canUnlockAll(boxes):
-    """Determine if all boxes can be opened."""
-    if not boxes or not isinstance(boxes, list):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list of lists): A list where each index represents a box,
+        and each box contains a list of keys.
+
+    Returns:
+        bool: True if all boxes can be opened, otherwise False.
+    """
+    if not isinstance(boxes, list) or len(boxes) == 0:
         return False
 
-    n = len(boxes)
-    opened = set([0])     # Box 0 is already open
-    keys = list(boxes[0]) # Keys found in box 0
+    opened = set([0])        # Box 0 is unlocked
+    keys = set(boxes[0])     # Keys found in box 0
 
     while keys:
         key = keys.pop()
 
-        if key < n and key not in opened:
+        if key < len(boxes) and key not in opened:
             opened.add(key)
-            # Add the keys from this newly opened box
-            for new_key in boxes[key]:
-                if new_key not in opened:
-                    keys.append(new_key)
+            # Add all new keys found in this newly opened box
+            for k in boxes[key]:
+                if k not in opened:
+                    keys.add(k)
 
-    return len(opened) == n
+    return len(opened) == len(boxes)
