3f2539c37dd7aa0a689cb70ad651ef95da81298e
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-12-05 03:15:49 -0500

    python for the first task

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,14 +5,15 @@ def canUnlockAll(boxes):
         return False
 
     n = len(boxes)
-    opened = set([0])
-    keys = list(boxes[0])
+    opened = set([0])     # Box 0 is already open
+    keys = list(boxes[0]) # Keys found in box 0
 
     while keys:
         key = keys.pop()
 
         if key < n and key not in opened:
             opened.add(key)
+            # Add the keys from this newly opened box
             for new_key in boxes[key]:
                 if new_key not in opened:
                     keys.append(new_key)
